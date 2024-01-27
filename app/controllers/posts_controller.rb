7f36b18b33cc279497d85a35bea72dd42a65806c
MARK RUBANZA <markrubanza10@gmail.com>
2024-01-28 02:10:04 +0300

    Add create new posts, comments and likes functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,29 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @current_user = current_user
+    @post = Post.find(params[:post_id])
+  end
+
+  def new
+    @current_user = current_user
+    @post = @current_user.posts.new
+  end
+
+  def create
+    @current_user = current_user
+    @post = @current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(@current_user), notice: 'Post created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
