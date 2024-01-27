a1f529e0ca4f02d63389b25de9afdbd2d070a76a
MARK RUBANZA <markrubanza10@gmail.com>
2024-01-28 02:13:32 +0300

    Fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,23 @@
 class LikesController < ApplicationController
-    before_action :set_post
+  before_action :set_post
 
-    def create
-      @like = @post.likes.new(like_params)
+  def create
+    @like = @post.likes.new(like_params)
 
-      if @like.save
-        redirect_to post_comments_path(current_user, @post), notice: 'Post liked.'
-      else
-        redirect_to post_comments_path(current_user, @post), alert: 'Failed to like post.'
-      end
-    end
-
-    private
-
-    def set_post
-      @post = Post.find(params[:post_id])
-    end
-
-    def like_params
-      params.permit(:author_id, :post_id)
+    if @like.save
+      redirect_to post_comments_path(current_user, @post), notice: 'Post liked.'
+    else
+      redirect_to post_comments_path(current_user, @post), alert: 'Failed to like post.'
     end
   end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def like_params
+    params.permit(:author_id, :post_id)
+  end
+end
